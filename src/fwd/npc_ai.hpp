34ff846b6f846bc06edbfbdc333c8167a20d003e
Julian Smythe <sausage@tehsausage.com>
2016-10-26 07:08:56 +1030

    missing header

---
+++ src/fwd/npc_ai.hpp
@@ -0,0 +1,17 @@
+
+/* $Id$
+ * EOSERV is released under the zlib license.
+ * See LICENSE.txt for more info.
+ */
+
+#ifndef FWD_NPC_AI_HPP_INCLUDED
+#define FWD_NPC_AI_HPP_INCLUDED
+
+class NPC_AI;
+
+class NPC_Path;
+
+class NPC_AI_Standard;
+class NPC_AI_Legacy;
+
+#endif // FWD_NPC_AI_HPP_INCLUDED
